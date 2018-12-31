918b393ce7dfd49885a9f3b185b725b654ff5b4f
Gabriel Cabrera <gcabrerag@fen.uchile.cl>
2018-12-30 22:08:43 -0300

    function added

---
+++ Asset_Allocation_Function.R
@@ -0,0 +1,169 @@
+#'---
+#' author: Gabriel Cabrera 
+#' title: Asset Allocation Function
+#' date: 30-12-2018  (last updated: 30-12-2018)
+#'---
+
+asset_allocation <- function(forecast_variable, predictor, in_sample_end, 
+                             start_sample, window, forecast_horizon, RRA, 
+                             w_LB, w_UB){
+  
+  h <- forecast_horizon
+  
+  # Take care of preliminaries -------------------------------------------------
+  T <- nrow(forecast_variable)
+  in_sample_end <- in_sample_end
+  R <- (in_sample_end  - start_sample)*12 # in-sample period 204
+  P <- T - R # out-of-sample period 300
+  
+  FC_PM <- matrix(NaN, nrow = P, ncol = NROW(h))
+  w_PM <- matrix(NaN, nrow = P, ncol = NROW(h))
+  R_PM <- matrix(NaN, nrow = P, ncol = NROW(h))
+  ER_PM <- matrix(NaN, nrow = P, ncol = NROW(h))
+  
+  FC_PR <- array(NaN,c(P, NCOL(predictor), NROW(h)))
+  w_PR <- array(NaN,c(P, NCOL(predictor), NROW(h)))
+  R_PR <- array(NaN,c(P, NCOL(predictor), NROW(h)))
+  ER_PR <- array(NaN,c(P, NCOL(predictor), NROW(h)))
+  
+  R_BH <- matrix(NaN, nrow = P, ncol = NROW(h))
+  ER_BH <- matrix(NaN, nrow = P, ncol = NROW(h))
+  FC_vol <- matrix(NaN, nrow = P, ncol = NROW(h))
+  
+  window <- 12*window
+  RRA <- 3
+  w_LB <- w_LB
+  w_UB <- w_UB
+  
+  # Compute out-of-sample forecasts --------------------------------------------
+  for(p in 1:P){
+    print(p)
+    for(j in 1:NROW(h)){
+      
+      # volatility
+      if(R+p-h[j] <= window-1){
+        
+        FC_vol[p,j] <- sd(ER_h[1:(R+p-h[j]),j])
+        
+      }else{
+        
+        FC_vol[p,j] <- sd(ER_h[((R+p-h[j])-(window-1)):(R+p-h[j]),j])
+        
+      }
+      
+      # Prevailing mean benchmark
+      FC_PM[p,j] <- mean(ER_h[1:(R+p-h[j]),j])
+      
+      # Predictive regressions
+      for(i in 1:(NCOL(predictor)+1)){
+        if(i <= NCOL(predictor)){
+          
+          X_i_j_p <- cbind(matrix(1, nrow = (R+(p-1)-h[j]), ncol = 1),
+                           predictor[1:(R+(p-1)-h[j])])
+          results_i_j_p <- lm(ER_h[2:(R+p-h[j]),j] ~ X_i_j_p[,2])
+          FC_PR[p,i,j] <- cbind(1, predictor[R+(p-1)])%*%results_i_j_p$coefficients
+          
+        }
+      }
+    }
+  }
+  
+  # Computing portfolio weights/returns ----------------------------------------
+  for(j in 1:NROW(h)){
+    for(t in 1:(P/h[j])){
+      
+      FC_vol_j_t <- FC_vol[((t-1)*h[j]+1),j]
+      FC_PM_j_t <- FC_PM[((t-1)*h[j]+1),j]
+      
+      w_PM_j_t <- (1/RRA)*FC_PM_j_t/(FC_vol_j_t)^2
+      
+      ifelse(w_PM_j_t > w_UB,
+             w_PM[((t-1)*h[j]+1),j] <- w_UB,
+             ifelse(w_PM_j_t < w_LB,
+                    w_PM[((t-1)*h[j]+1),j] <- w_LB,
+                    w_PM[((t-1)*h[j]+1),j] <- w_PM_j_t))
+      
+      R_PM[((t-1)*h[j]+1),j] <- R_f_h[(R+(t-1)*h[j]+1),j] + w_PM[((t-1)*h[j]+1),j]*ER_h[(R+(t-1)*h[j]+1),j]
+      ER_PM[((t-1)*h[j]+1),j] <- R_PM[((t-1)*h[j]+1),j] - R_f_h[(R+(t-1)*h[j]+1),j]
+      
+      for(i in 1:ncol(FC_PR)){
+        
+        FC_PR_i_j_t <- FC_PR[((t-1)*h[j]+1),i,j]
+        w_PR_i_j_t <- (1/RRA)*FC_PR_i_j_t/(FC_vol_j_t)^2
+        
+        ifelse(w_PR_i_j_t > w_UB,
+               w_PR[((t-1)*h[j]+1),i,j] <- w_UB,
+               ifelse(w_PR_i_j_t < w_LB,
+                      w_PR[((t-1)*h[j]+1),i,j] <- w_LB,
+                      w_PR[((t-1)*h[j]+1),i,j] <- w_PR_i_j_t))
+        
+        R_PR[((t-1)*h[j]+1),i,j] <- R_f_h[(R+(t-1)*h[j]+1),j] + w_PR[((t-1)*h[j]+1),i,j]*ER_h[(R+(t-1)*h[j]+1),j]
+        ER_PR[((t-1)*h[j]+1),i,j] <- R_PR[((t-1)*h[j]+1),i,j] - R_f_h[(R+(t-1)*h[j]+1),j]
+        
+      }
+      
+      R_BH[((t-1)*h[j]+1),j] <- R_f_h[(R+(t-1)*h[j]+1),j] + ER_h[(R+(t-1)*h[j]+1),j]
+      ER_BH[((t-1)*h[j]+1),j] <- ER_h[(R+(t-1)*h[j]+1),j]
+      
+    }
+  }
+  
+  
+  # Compute CER gains and Sharpe ratios for full OOS period --------------------
+  CER_gain <- matrix(NaN, nrow = (ncol(FC_PR)+1), ncol = NROW(h))
+  Sharpe <- matrix(NaN, nrow = (ncol(FC_PR)+2), ncol = NROW(h))
+  mean <- matrix(NaN, nrow = (ncol(FC_PR)+2), ncol = NROW(h)) 
+  st_deviation <- matrix(NaN, nrow = (ncol(FC_PR)+2), ncol = NROW(h)) 
+  CER_PREDIC <- matrix(NaN, nrow = (ncol(FC_PR)), ncol = NROW(h))
+  CER_BENCH <- matrix(NaN, nrow = (ncol(FC_PR)), ncol = NROW(h))
+  
+  for(j in 1:NROW(h)){
+    
+    R_PM_j <- R_PM[,j]
+    R_PM_j <- na.omit(R_PM_j)
+    ER_PM_j <- ER_PM[,j]
+    ER_PM_j <- na.omit(ER_PM_j)
+    CER_PM_j <- (12/h[j])*(mean(R_PM_j) - 0.5*RRA*sd(R_PM_j)^2)
+    Sharpe[1,j] <- sqrt((12/h[j]))*mean(ER_PM_j)/sd(ER_PM_j)
+    
+    mean[1,j] <- mean(ER_PM_j)  
+    st_deviation[1,j] <- sd(ER_PM_j)  
+    CER_BENCH[1,j] <- CER_PM_j 
+    
+    for(i in 1:NCOL(FC_PR)){
+      
+      R_PR_i_j <- R_PR[,i,j]
+      R_PR_i_j <- na.omit(R_PR_i_j)
+      ER_PR_i_j <- ER_PR[,i,j]
+      ER_PR_i_j <- na.omit(ER_PR_i_j)
+      CER_PR_i_j <- (12/h[j])*(mean(R_PR_i_j) - 0.5*RRA*sd(R_PR_i_j)^2)
+      CER_gain[i,j] <- 100*(CER_PR_i_j - CER_PM_j)
+      Sharpe[i+1,j] <- sqrt((12/h[j]))*mean(ER_PR_i_j)/sd(ER_PR_i_j)
+      
+      mean[i+1,j] <- mean(ER_PR_i_j)  
+      st_deviation[i+1,j] <- sd(ER_PR_i_j)  
+      CER_PREDIC[i,j] <- CER_PR_i_j
+      
+    }
+    
+    R_BH_j <- R_BH[,j]
+    R_BH_j <- na.omit(R_BH_j)
+    ER_BH_j <- ER_BH[,j]
+    ER_BH_j <- na.omit(ER_BH_j)
+    CER_BH_j <- (12/h[j])*(mean(R_BH_j) - 0.5*RRA*sd(R_BH_j)^2)
+    CER_gain[NROW(CER_gain),j] <- 100*(CER_BH_j - CER_PM_j)
+    Sharpe[NROW(Sharpe),j] <- sqrt((12/h[j]))*mean(ER_BH_j)/sd(ER_BH_j)
+    
+    mean[NROW(mean),j] <- mean(ER_BH_j)  
+    st_deviation[NROW(st_deviation),j] <- sd(ER_BH_j)  
+    
+  }
+  
+  CER_gain_results <<- CER_gain
+  Sharpe_results <<- Sharpe 
+  mean_results <<- mean
+  Std_results <<- st_deviation  
+  CER_BENCH_results <<-  CER_BENCH  
+  CER_PREDIC_results <<-  CER_PREDIC
+  
+}
---
+++ Asset_Allocation_Implementation.R
@@ -0,0 +1,104 @@
+#'---
+#' author: Gabriel Cabrera 
+#' title: Asset Allocation Implementation
+#' date: 30-12-2018  (last updated: 30-12-2018)
+#'---
+
+if(!require("pacman")) install.packages("pacman")
+p_load("tidyverse", "xlsx")
+
+GW_dataset <- read.xlsx("data/Returns_short_interest_data.xlsx", 
+                        sheetName = "GW variables") 
+
+GW_varset <- as.data.frame(apply(GW_dataset, 2, as.numeric)) %>% 
+             filter(yyyymm >= 197212) %>% 
+             select(yyyymm, Index, D12, E12, b.m, ntis, tbl, lty, ltr, BAA, 
+               AAA, corpr, infl) %>%
+             rename(SP = Index, D12 = D12, E12 = E12, BM = b.m, NTIS = ntis,
+               TBL = tbl, LTY = lty, LTR = ltr, BAA = BAA, AAA = AAA,
+               CORPR = corpr, INFL_lag = infl) %>%  
+             mutate(log_DP = log(D12/SP), log_EP = log(E12/SP), 
+               log_DE = log(D12/E12), log_DY = log(D12/lag(SP)),
+               TMS = LTY - TBL, DFY = BAA - AAA, DFR = CORPR - LTR,
+               INFL_lag = lag(INFL_lag)) %>% 
+             select(log_DP, log_DY, log_EP, log_DE, BM, NTIS, TBL, LTY, 
+               LTR, TMS, DFY, DFR, INFL_lag) %>% 
+             na.omit()
+
+# Stock excess return volatility (annualized) ----------------------------------
+RVOL <- as.data.frame(apply(GW_dataset, 2, as.numeric)) %>% 
+        filter(yyyymm >= 197201) %>% 
+        select(yyyymm, CRSP_SPvw, Rfree) %>% 
+        mutate(R_F_lag = lag(Rfree)) %>% 
+        rename(SP_R = CRSP_SPvw) %>% 
+        select(yyyymm, SP_R, R_F_lag) %>% 
+        na.omit() 
+
+RVOL_mat <- matrix(NaN, nrow = nrow(RVOL) - 11, ncol = 1)
+
+for(i in seq_along(RVOL_mat)){
+  
+  RVOL_mat[i,] <- mean(abs(RVOL$SP_R[i:(i+11)] - RVOL$R_F_lag[i:(i+11)]))
+  
+}
+
+RVOL_mat <- sqrt(pi/2)*sqrt(12)*RVOL_mat
+
+# merge variables --------------------------------------------------------------
+GW_predictor <- cbind(GW_varset, RVOL_mat) %>% 
+                rename(RVOL = RVOL_mat) %>% 
+                select(log_DP, log_DY, log_EP, log_DE, RVOL, BM, NTIS, TBL, LTY, 
+                       LTR, TMS, DFY, DFR, INFL_lag)
+
+rm(GW_varset, RVOL, RVOL_mat)
+
+# Load equity risk premium data, 1973:01-2014:12 -------------------------------
+equity_risk <- data.frame(apply(GW_dataset, 2, as.numeric)) %>% 
+               filter(yyyymm >= 197212) %>% 
+               select(yyyymm, Rfree, CRSP_SPvw) %>%
+               rename(R_SP500 = CRSP_SPvw) %>% 
+               mutate(ER = R_SP500 - lag(Rfree), Rfree_lag = lag(Rfree)) %>% 
+               na.omit()
+
+# short-interest rate ----------------------------------------------------------
+short_interest <- read.xlsx("data/Returns_short_interest_data.xlsx", 
+                             sheetName = "Short interest") %>% 
+                  select(Date, EWSI) %>% 
+                  mutate(log_EWSI = log(EWSI)) %>% 
+                  select(Date, log_EWSI)
+
+# Compute cumulative excess returns --------------------------------------------
+h <- c(1, 3, 6, 12)
+
+ER_h <- matrix(NaN, nrow = nrow(equity_risk), ncol = NROW(h))
+R_f_h <- matrix(NaN, nrow = nrow(equity_risk), ncol = NROW(h))
+
+for(i in seq_along(h)){
+  for(j in 1:(nrow(equity_risk) - (h[i] - 1))){
+    
+    ER_h[j,i] <- prod(1 + equity_risk$R_SP500[j:(j+h[i]-1)]) - prod(1 + equity_risk$Rfree_lag[j:(j+h[i]-1)])
+    R_f_h[j,i] <- prod(1 + equity_risk$Rfree_lag[j:(j+h[i]-1)]) - 1
+    
+  }
+}
+
+
+# Load function Aset Allocation ------------------------------------------------
+source("Asset_Allocation_Function.R")
+
+asset_allocation(equity_risk, GW_predictor[,1], 1989, 1972, 10, h, 3, -0.5, 1.5)
+
+CER_BENCH_results
+CER_gain_results
+CER_PREDIC_results
+Std_results
+mean_results
+
+results <- matrix(0, nrow = 1, ncol = 10)
+
+for(i in 1:1){
+  
+  results
+  
+}
+
